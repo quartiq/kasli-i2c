01f94d471764152e4be6cec477cfa8f464c1c49c
SingularitySurfer <Norman_Krackow@gmx.de>
2020-11-18 12:39:51 +0000

    modified for quartiq workflows

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
 
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -d|--dir)
+    DIR="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -v|--version)
+    VERSION="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)
+esac
+done
+
 set -e
 set -x
 
-SERIAL=$(python kasli_get_mac.py)
+SERIAL=$(python3 kasli_get_mac.py)
 FT_SERIAL=$SERIAL
+
 
 {
 
 IP=10.34.16.100
 FT_BUS=1
-FT_PORT=7.2
+FT_PORT=7.4
 
 UART_DEV=/dev/serial/by-id/usb-ARTIQ_Sinara_Quad_RS232-HS_$FT_SERIAL-if02-port0
 BUSNUM=$(cat /sys/bus/usb/devices/$FT_BUS-$FT_PORT/busnum)
@@ -20,10 +40,10 @@ cat kasli-ft4232h.conf.in | m4 -DFT_SERIAL=$FT_SERIAL > kasli-ft4232h.conf
 ftdi_eeprom --device d:$BUSNUM/$DEVNUM --flash-eeprom kasli-ft4232h.conf
 sleep 3
 artiq_mkfs storage.img -s ip $IP
-artiq_flash -t kasli -I "ftdi_serial $FT_SERIAL" -V tester --srcbuild -d artiq_kasli -f storage.img gateware bootloader firmware storage start
+artiq_flash -t kasli -I "ftdi_serial $FT_SERIAL" -V $VERSION --srcbuild -d $DIR -f storage.img gateware bootloader firmware storage start
 stty -F $UART_DEV 115200 cs8 -cstopb -parenb opost onlcr
 timeout --foreground 15 socat stdio $UART_DEV || true
-sudo arp -d $IP || true
+#sudo arp -d $IP || true
 ping -w40 -W40 -c4 $IP
 echo SUCCESS
 
