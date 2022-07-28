d4cd9859eab116c12a9e5a0c1233085bfe3d543a
SingularitySurfer <Norman_Krackow@gmx.de>
2022-07-28 07:25:28 +0000

    change artiq_flash to work with artiq 7 and afws built files

--- deploy.sh
+++ deploy.sh
@@ -40,7 +40,7 @@ cat kasli-ft4232h.conf.in | m4 -DFT_SERIAL=$FT_SERIAL > kasli-ft4232h.conf
 ftdi_eeprom --device d:$BUSNUM/$DEVNUM --flash-eeprom kasli-ft4232h.conf
 sleep 3
 artiq_mkfs storage.img -s ip $IP
-artiq_flash -t kasli -I "ftdi_serial $FT_SERIAL" -V $VERSION --srcbuild -d $DIR -f storage.img gateware bootloader firmware storage start
+artiq_flash -t kasli -I "ftdi_serial $FT_SERIAL" -d $DIR -f storage.img gateware bootloader firmware storage start
 stty -F $UART_DEV 115200 cs8 -cstopb -parenb opost onlcr
 timeout --foreground 15 socat stdio $UART_DEV || true
 sudo ip neigh flush to $IP || true
