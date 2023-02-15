83b30643880dba2bddb74f746326e994cfe69144
SingularitySurfer <Norman_Krackow@gmx.de>
2023-02-15 15:32:29 +0000

    change to new murray USB topo

--- deploy.sh
+++ deploy.sh
@@ -30,7 +30,7 @@ FT_SERIAL=$SERIAL
 
 IP=10.34.16.100
 FT_BUS=1
-FT_PORT=7.4
+FT_PORT=8.3
 
 UART_DEV=/dev/serial/by-id/usb-ARTIQ_Sinara_Quad_RS232-HS_$FT_SERIAL-if02-port0
 BUSNUM=$(cat /sys/bus/usb/devices/$FT_BUS-$FT_PORT/busnum)
@@ -40,7 +40,7 @@ cat kasli-ft4232h.conf.in | m4 -DFT_SERIAL=$FT_SERIAL > kasli-ft4232h.conf
 ftdi_eeprom --device d:$BUSNUM/$DEVNUM --flash-eeprom kasli-ft4232h.conf
 sleep 3
 artiq_mkfs storage.img -s ip $IP
-artiq_flash -t kasli -I "ftdi_serial $FT_SERIAL" -V $VERSION --srcbuild -d $DIR -f storage.img gateware bootloader firmware storage start
+artiq_flash -t kasli -I "ftdi_serial $FT_SERIAL" -d $DIR -f storage.img gateware bootloader firmware storage start
 stty -F $UART_DEV 115200 cs8 -cstopb -parenb opost onlcr
 timeout --foreground 15 socat stdio $UART_DEV || true
 sudo ip neigh flush to $IP || true
