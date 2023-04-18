057832c4db7d5a86fa6d9c420bf5f76c45a425fd
SingularitySurfer <Norman_Krackow@gmx.de>
2023-04-18 13:26:00 +0000

    add srcbuild flag

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SRC=false
+
 while [[ $# -gt 0 ]]
 do
 key="$1"
@@ -15,7 +17,13 @@ case $key in
     shift # past argument
     shift # past value
     ;;
+    --src)
+    SRC=true
+    shift # past argument
+    shift # past value
+    ;;
     *)
+    -
 esac
 done
 
@@ -30,7 +38,7 @@ FT_SERIAL=$SERIAL
 
 IP=10.34.16.100
 FT_BUS=1
-FT_PORT=8.3
+FT_PORT=4.4.2
 
 UART_DEV=/dev/serial/by-id/usb-ARTIQ_Sinara_Quad_RS232-HS_$FT_SERIAL-if02-port0
 BUSNUM=$(cat /sys/bus/usb/devices/$FT_BUS-$FT_PORT/busnum)
@@ -40,7 +48,11 @@ cat kasli-ft4232h.conf.in | m4 -DFT_SERIAL=$FT_SERIAL > kasli-ft4232h.conf
 ftdi_eeprom --device d:$BUSNUM/$DEVNUM --flash-eeprom kasli-ft4232h.conf
 sleep 3
 artiq_mkfs storage.img -s ip $IP
-artiq_flash -t kasli -I "ftdi_serial $FT_SERIAL" -d $DIR -f storage.img gateware bootloader firmware storage start
+if [ "$SRC" = true ]; then
+    artiq_flash -t kasli -I "ftdi_serial $FT_SERIAL" -d $DIR --srcbuild -f storage.img gateware bootloader firmware storage start
+else
+    artiq_flash -t kasli -I "ftdi_serial $FT_SERIAL" -d $DIR  -f storage.img gateware bootloader firmware storage start
+fi
 stty -F $UART_DEV 115200 cs8 -cstopb -parenb opost onlcr
 timeout --foreground 15 socat stdio $UART_DEV || true
 sudo ip neigh flush to $IP || true
